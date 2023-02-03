4ba7b3a0b63227093043d7519acd8cc48261ef92
Josh <lutz_143@yahoo.com>
2023-02-03 14:44:23 -0500

    added utils folder with auth and formatting helpers for our views

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
-const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth,async (req, res) => {
---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+module.exports = {
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+  format_time: (date) => {
+    // format time
+    return date.toLocaleTimeString();
+  },
+};
