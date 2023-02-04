4464bd36bddb4514bb91ccad4beaf47f8eed0824
Josh <lutz_143@yahoo.com>
2023-02-04 09:09:04 -0500

    created initial handlebars from test index and css files

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,17 +3,17 @@ const { Post } = require('../models');
 const postSeeds = [
   {
     title: "Apple Watch",
-    post: "I just purchased an Apple Watch and realized that I can follow my favorite developer handles on Reddit. Does anyone else do the same? Would love to hear your thoughts!",
+    post_content: "I just purchased an Apple Watch and realized that I can follow my favorite developer handles on Reddit. Does anyone else do the same? Would love to hear your thoughts!",
     user_id: 3
   },
   {
     title: "News",
-    post: "I want to hear from the development community about what news outlets we all follow. I am interested in creating a website that grabs the most happy and upbeat news of the day so we're not bombarded by sad and frightening news all of the time.",
+    post_content: "I want to hear from the development community about what news outlets we all follow. I am interested in creating a website that grabs the most happy and upbeat news of the day so we're not bombarded by sad and frightening news all of the time.",
     user_id: 1
   },
   {
     title: "MVC",
-    post: "MVC is an architectural pattern that structures a codebase in three distinct sections, according to a software design philosophy known as the separation of concerns.",
+    post_content: "MVC is an architectural pattern that structures a codebase in three distinct sections, according to a software design philosophy known as the separation of concerns.",
     user_id: 2
   }
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,18 @@
 module.exports = {
+  // format_date: (date) => {
+  //   // Format date as MM/DD/YYYY
+  //   return date.toLocaleDateString();
+  // },
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    // Using JavaScript Date methods, we get and format the month, date, and year
+    // We need to add one to the month since it is returned as a zero-based value
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+      // We add five years to the 'year' value to calculate the end date
+      new Date(date).getFullYear() + 5
+    }`;
   },
-  format_time: (date) => {
-    // format time
-    return date.toLocaleTimeString();
-  },
+  // format_time: (date) => {
+  //   // format time
+  //   return date.toLocaleTimeString();
+  // },
 };
