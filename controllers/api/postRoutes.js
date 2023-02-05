3cf20645217e88adde2ca659e99adb50a75ee5dc
Josh <lutz_143@yahoo.com>
2023-02-05 13:38:25 -0500

    updates to styling and html for proper blog rendering

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,6 +16,27 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
@@ -26,7 +47,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!postData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
